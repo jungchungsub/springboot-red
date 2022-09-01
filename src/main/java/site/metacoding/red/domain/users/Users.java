df7021efe2a76e7f5ef8c9d77e63107e7a168b57
jungchungsub <cndtjq1248@naver.com>
2022-09-01 15:43:04 +0900

    mybatis 최종

--- src/main/java/site/metacoding/red/domain/users/Users.java
+++ src/main/java/site/metacoding/red/domain/users/Users.java
@@ -4,13 +4,26 @@ import java.sql.Timestamp;
 
 import lombok.Getter;
 import lombok.Setter;
+import site.metacoding.red.web.dto.request.users.UpdateDto;
 
-@Setter
+
 @Getter
+
 public class Users {
 	private Integer id;
 	private String username;
 	private String password;
 	private String email;
 	private Timestamp createdAt;
+	
+	public void 패스워드수정(String password) {
+		this.password = password;
+	}
+	
+	public void 전체수정(UpdateDto updateDto) {
+		this.username = updateDto.getUsername();
+		this.password = updateDto.getPassword();
+		this.email = updateDto.getEmail();
+	}
+
 }
--- src/main/java/site/metacoding/red/domain/users/UsersDao.java
+++ src/main/java/site/metacoding/red/domain/users/UsersDao.java
@@ -1,6 +1,16 @@
 package site.metacoding.red.domain.users;
 
+import java.util.List;
+
+import site.metacoding.red.web.dto.request.users.JoinDto;
+import site.metacoding.red.web.dto.request.users.UpdateDto;
+
+
 public interface UsersDao {
-	public void insert(Users user);
+	public void insert(JoinDto joinDto);
 	public Users findById(Integer id);
+	public List<Users> findAll();
+	public void delete(Integer id);
+	
+	public void update(Users users);
 }
